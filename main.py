3629dae0f51c57e53b794cfa5fbe59d91851ab2d
Jad Kais <jadk1992@gmail.com>
2024-06-02 23:57:57 +0930

    Prim Algorithm complete

--- main.py
+++ main.py
@@ -9,12 +9,56 @@ def letter_to_number(letter):
 
 # Create nodes 
 class Graph:
-    def _init_(self,country,build,destroy,vertices):
+    def __init__(self,country,build,destroy,vertices):
         self.V = vertices # Vertices as each country/buil/destroy will be a square
-        self.finalcost = 0
-        self.graph = [[] for _ in range(vertices)]
+        self.totalcost = 0 # Init final cost that will be
+        self.startingcost = 0 # Init cost for new graph  
+        self.graph = [[] for _ in range(vertices)] # Init a graph that is empty for the mst
 
-        
+        for i in range(vertices):
+            for j in range(i, vertices):
+                if country[i][j] == 0:
+                    self.edge(i,j,build[i][j])
+                else:
+                    self.edge(i,j, -destroy[i][j])
+                    self.startingcost += destroy[i][j]
+
+    def edge(self, startingnode, node, cost):
+        self.graph[startingnode].append((node,cost))    
+        self.graph[node].append((startingnode,cost))    
+
+    def prim_alg(self):
+        pQueue = [] # init a priority queue that will store the edge costs of the surrounding nodes
+        srcCity = 0 # init a inital node/source node that will act as the starting point for the MST
+
+        travelled = [] # (mst set) init a set that will store all the nodes that have been visited
+
+        cost = [float('inf')] * self.V # init a "cost" list and all inital costs are infinty ('inf')
+
+        heapq.heappush(pQueue, (0, srcCity))
+        cost[srcCity] = 0
+
+        while pQueue: # While the priority queue is not empty
+            # add the current node stored in pQueue to the current city
+            currentCost, currentCity = heapq.heappop(pQueue)
+
+            if currentCity in travelled: # check if the currentCity has already been visited
+                continue
+            
+            travelled.append(currentCity) # add the currentCity to the list of travelled city
+            
+            self.totalcost += currentCost
+
+            for node,node_cost in self.graph[currentCity]:
+                
+                if node not in travelled and cost[node] > node_cost:
+                    cost[node] = node_cost
+                    heapq.heappush(pQueue, (cost[node], node))
+
+        self.totalcost = self.startingcost + self.totalcost
+
+        return self.totalcost
+      
 
 # Taking in inputs
 input = input()
@@ -26,10 +70,11 @@ country = [[int(x) for x in str(part)] for part in country_str.split(",")]
 build = [[letter_to_number(x) for x in part] for part in build_str.split(",")]
 destroy = [[letter_to_number(x) for x in part] for part in destroy_str.split(",")]
 
+
 vertices = len(country)
 
-g = Graph(country,build,destroy,vertices)
+g = Graph(country, build, destroy, vertices)
+g.prim_alg()
+print(g.totalcost)
 
-print()
-
-# 111,111,111 ABC,ABC,ABC abc,abc,abc
+# 011,101,110 ABD,BAC,DCA ABD,BAC,DCA
